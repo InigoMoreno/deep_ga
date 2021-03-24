407d2d61722d0e34c07309adbad1c1973c4cc565
ImInSpace <ignigomoreno@gmail.com>
2021-03-24 09:22:40 +0100

    Added get_gps_references

--- deep_ga/__init__.py
+++ deep_ga/__init__.py
@@ -1,3 +1,4 @@
 from deep_ga.functions import ply_to_image
 from deep_ga.patch_generator import get_patch, get_batch, PatchDataGenerator
 from deep_ga.layers import SymConv2D, EuclideanDistanceLayer
+from deep_ga.constants import get_gps_references
