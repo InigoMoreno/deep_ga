7975999aa584539e13bf8d1fb00d7debae58f3a8
ImInSpace <ignigomoreno@gmail.com>
2021-03-31 08:48:02 +0200

    Added missing functions to init

--- deep_ga/__init__.py
+++ deep_ga/__init__.py
@@ -1,4 +1,4 @@
 from deep_ga.functions import ply_to_image, get_valid_filename
-from deep_ga.patch_generator import get_patch, get_batch, PatchDataGenerator
+from deep_ga.patch_generator import get_patch, get_batch, PatchDataGenerator, get_batch_local_global, LocalGlobalPatchDataGenerator
 from deep_ga.layers import SymConv2D, EuclideanDistanceLayer
 from deep_ga.constants import get_gps_references
