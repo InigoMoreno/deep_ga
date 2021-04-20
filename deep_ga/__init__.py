69c63caafa5bcf31b2c6dc4f2ea100f23e39deb0
ImInSpace <ignigomoreno@gmail.com>
2021-04-20 10:15:44 +0200

    Fix bug

--- deep_ga/__init__.py
+++ deep_ga/__init__.py
@@ -5,13 +5,3 @@ from deep_ga.constants import get_gps_references
 from deep_ga.keras_utils import get_all_layers, count_weights, try_copying_weights, are_models_equal, find_equal_model
 from deep_ga.mdnt import SWATS
 from deep_ga.losses import set_scale, pairwise_contrastive_loss, binary_cross_entropy, doomloss
-
-custom_objects = {
-    "SymConv2D": layers.SymConv2D,
-    "PConv2D": layers.PConv2D,
-    "EuclideanDistanceLayer": layers.EuclideanDistanceLayer,
-    "NanToZero": layers.NanToZero,
-    "pairwise_contrastive_loss": losses.pairwise_contrastive_loss,
-    "binary_cross_entropy": losses.binary_cross_entropy,
-    "doomloss": losses.doomloss
-}
